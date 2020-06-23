8194b09b95249e99243953b16ebf580c023039be
Jeff Perkinson <jmperkinson@gmail.com>
2020-06-23 17:05:21 -0400

    Add Category model, migration, and define relationships.

--- app/Category.php
+++ app/Category.php
@@ -4,27 +4,26 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Todo extends Model
+class Category extends Model
 {
-     /**
+    /**
      * The table associated with the model.
      *
      * @var string
      */
-    protected $table = 'todos';
+    protected $table = 'categories';
 
     // primary key 'id' is assumed by Eloquent, as well as
     // created_at and updated_at fields for the todos table
 
     // Fields that are allowed mass assignment
     protected $fillable = [
-        'name',
-        'status'
+        'category',
     ];
 
-    // Todo-Category relationship
-    public function category() 
+    // Category-Todo relationship
+    public function todos() 
     {   
-        return $this->belongsTo(Category::class);
+        return $this->hasMany(Todo::class);
     }
 }
