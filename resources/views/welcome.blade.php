e20c887d768a4194745e9d32fba84f66a29deb4b
Jeff Perkinson <jmperkinson@gmail.com>
2020-06-25 20:56:06 -0400

    Add todos link to Welcome view.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel REST API</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,18 +81,23 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel REST API
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
+                    <a href="{{env('app_url')}}/api/todos/" target="_blank">Todos List</a>
+                    <!-- JSON formatter -->
+                    <a href="https://jsonformatter.curiousconcept.com/"
+                    target="_blank">JSON Formatter</a>
+                    <!-- default Laravel Links -->
+                    <!-- <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a> -->
                 </div>
             </div>
         </div>
